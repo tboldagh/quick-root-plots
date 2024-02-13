244c7c8e66d72e1c44d14067a3e9e90678fbd881
tboldagh <tomasz.bold@gmail.com>
2024-02-13 20:48:39 +0100

    Restored TABed browsing script

---
+++ scripts/browse.py
@@ -0,0 +1,26 @@
+global _file0
+from fast import *
+class r(object):
+    def __init__(self, dir, path=""):        
+        def mkpath(prefix, newpart):
+            return prefix+"/"+newpart if prefix else newpart
+        
+        for k in list(dir.GetListOfKeys()):            
+            dirname  = k.GetName().replace("-","_")
+            subpath = mkpath(path, k.GetName())
+            if k.GetClassName() == 'TDirectoryFile':
+                print("/", end="")
+                subdir = get(_file0, subpath)
+                setattr(self, dirname, r(subdir, subpath))
+            else:
+                print(".", end="")
+                setattr(self, dirname, get(_file0, subpath))
+
+print(". indexing file content (every / is dir, every * is object )")
+ro = r(_file0)
+print(".. done ")
+cnv()
+
+print(". this is interactive browser of the ROOT file")
+print(". to browse file with tab completion start with typing: ro. and hit <TAB>")
+
