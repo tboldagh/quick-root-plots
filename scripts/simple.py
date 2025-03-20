923148619796cbedba13e37ba403e91f04eb682d
tboldagh <tomasz.bold@gmail.com>
2025-03-20 07:49:36 +0100

    Control over plots dimension

--- scripts/simple.py
+++ scripts/simple.py
@@ -5,11 +5,16 @@ options.help=False
 options.hist="_NOT_GIVEN_"
 options.xtit=None # axes titles
 options.ytit=None   
+options.yrange=None   
+
 options.drawopt="hp" # see: fast.py draw fuction, this option can be a list, this case options are specified for each hist
 options.out=None
 options.rmargin=None
 options.internal="tr" # add ATLAS internal labels
 options.msgsz=0.03
+options.msg=[] # additional messages
+options.msgpos=(0.6, 0.89)
+
 
 
 from fast import *
@@ -32,6 +37,23 @@ if options.internal:
     args = (options.internal) +("#font[72]{ATLAS} #font[42]{Internal}", options.msgsz)
     putlabel(*args)
 ccnv().Update()
+
+if isinstance(options.msgpos, str):
+    xbl, ybl, xtr, ytr = posKey2Abs(options.msgpos)
+    options.msgpos = (xbl, ytr)
+
+texts = []
+for index,m in enumerate(options.msg,1):
+    if isinstance(m, tuple):
+        texts.append( putlabel(*m) )
+    else:
+        texts.append( putlabel(options.msgpos[0], options.msgpos[1]-(options.msgsz+0.01)*index, m, options.msgsz) )
+
+if options.yrange:
+    h.GetYaxis().SetRangeUser(options.yrange[1], options.yrange[2])
+ccnv().Modified()
+ccnv().Update()
+
 
 if options.out:
     save(options.out)
